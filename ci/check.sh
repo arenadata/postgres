2d71c6415e7147bbc3c5d0304d9caa4877dd76e0
Egor Ivkov <e.o.ivkov@gmail.com>
2025-04-09 15:10:34 +0300

    Add scripts for gitlab ci
    orioledb scripts taken for reference

---
+++ ci/check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+# unsets limit for coredumps size
+ulimit -c unlimited -S
+# sets a coredump file pattern
+mkdir -p /tmp/cores-$CI_COMMIT_SHA-$TIMESTAMP
+sudo sh -c "echo \"/tmp/cores-$CI_COMMIT_SHA-$TIMESTAMP/%t_%p_%s.core\" > /proc/sys/kernel/core_pattern"
+
+make check-world -j4
