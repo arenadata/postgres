70030321d496ffdd3c2b54d2bf3849dd0c4a41ec
Egor Ivkov <e.o.ivkov@gmail.com>
2025-04-22 20:38:00 +0300

    Fix initdb error in CI

--- ci/check.sh
+++ ci/check.sh
@@ -5,4 +5,6 @@ set -eu
 # unsets limit for coredumps size
 ulimit -c unlimited -S
 
-make check-world -j4
+# initdb has to be run as non-root user
+useradd -m postgres
+su - postgres -c "cd $(pwd) && PATH=\"$PATH\" make check-world -j4"
