fed8463e0aaf35f7c029060a8b9a1f90f19e04e4
Egor Ivkov <e.o.ivkov@gmail.com>
2025-04-22 20:21:19 +0300

    Fix handling of core dumps

--- ci/check.sh
+++ ci/check.sh
@@ -4,8 +4,5 @@ set -eu
 
 # unsets limit for coredumps size
 ulimit -c unlimited -S
-# sets a coredump file pattern
-mkdir -p /tmp/cores-$CI_COMMIT_SHA-$CI_PIPELINE_CREATED_AT
-sh -c "echo \"/tmp/cores-$CI_COMMIT_SHA-$CI_PIPELINE_CREATED_AT/%t_%p_%s.core\" > /proc/sys/kernel/core_pattern"
 
 make check-world -j4
